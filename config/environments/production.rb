75033a345cb6d8eea570cc7fc5dc7eb506db0256
Jaz Lim <angmeng@gmail.com>
2013-11-15 13:08:40 +0800

    Add smtp setting and exception_notification setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,15 +13,19 @@ config.action_view.cache_template_loading            = true
 #ExceptionNotifier.sender_address       = %("APP Error" <yeongwei.tan@al-lighting.com>) # Replace APP with Application Name
 #ExceptionNotifier.email_prefix         = "[APP] "                               # Replace APP with Application Name
 #ExceptionNotifier.consider_local       = "132.241.xxx.xxx"                      # Accepts comma separated list of IPs
-
+ExceptionNotification::Notifier.exception_recipients = %w(laykuan.teh@al-lighting.com lim.am@icode-solution.com)
+# defaults to exception.notifier@default.com
+ExceptionNotification::Notifier.sender_address = %("Application Error" <app.error@al-lighting.com>)
+# defaults to "[ERROR] "
+ExceptionNotification::Notifier.email_prefix = "[APP Error] "
 
 # See everything in the log (default is :info)
- config.log_level = :debug
+config.log_level = :error
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 # config.logger = Logger.new(config.log_path, 50, 1.megabyte)
-config.logger = Logger.new(config.log_path, 'weekly')
+config.logger = Logger.new(config.log_path, 'daily')
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
@@ -34,3 +38,15 @@ config.logger = Logger.new(config.log_path, 'weekly')
 
 # Enable threaded mode
 # config.threadsafe!
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    :address => '10.102.4.25',
+    :port=>25,
+    :domain=>'al-lighting.com',
+    :user_name=>'laykuan.teh',
+    :password=>'Al@72088',
+    #:authentication=>:none,
+    # :enable_starttls_auto => true
+  }
