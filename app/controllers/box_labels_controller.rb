7fb6a8cb74c3bee072925364978ebbec8a376bb9
Jaz Lim <angmeng@gmail.com>
2014-02-04 16:06:42 +0800

    Redirect to index after print label

--- app/controllers/box_labels_controller.rb
+++ app/controllers/box_labels_controller.rb
@@ -1,6 +1,7 @@
 class BoxLabelsController < ApplicationController
 	before_filter :authenticate_admin
   def index
+    session[:label_index] = request.fullpath
   	# if params[:search] 
   	#   params[:search][:boxed_date_time_less_than_or_equal_to] = params[:search][:boxed_date_time_less_than_or_equal_to].to_date.strftime("%Y-%m-%d 23:59:59") if params[:search][:boxed_date_time_less_than_or_equal_to] && !params[:search][:boxed_date_time_less_than_or_equal_to].blank?
   	#   params[:search][:boxed_date_time_greater_than_or_equal_to] = params[:search][:boxed_date_time_greater_than_or_equal_to].to_date.strftime("%Y-%m-%d 00:00:00") if params[:search][:boxed_date_time_greater_than_or_equal_to] && !params[:search][:boxed_date_time_greater_than_or_equal_to].blank?
@@ -43,6 +44,8 @@ class BoxLabelsController < ApplicationController
   def print_barcode
   	@box = BoxLabel.find params[:id]
   	LabelEngine.new(@box).generate_label_content
+    flash[:notice] = "Label has been sent to printing."
+    redirect_to :action => "index"
   end
 
 end
